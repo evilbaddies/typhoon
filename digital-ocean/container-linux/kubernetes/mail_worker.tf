9433688687bf47b922fa07d7862f0b2a98ef01ad
erron <erronsilver@gmail.com>
2018-03-08 19:06:13 +0200

    Added dns reord for mail worker

--- digital-ocean/container-linux/kubernetes/mail_worker.tf
+++ digital-ocean/container-linux/kubernetes/mail_worker.tf
@@ -1,6 +1,17 @@
-# Tag to label workers
+# Tag to label mail worker
 resource "digitalocean_tag" "mail_worker" {
   name = "${var.cluster_name}-mail-worker"
+}
+
+# Mail worker DNS record
+resource "digitalocean_record" "mail_worker" {
+  # DNS zone where record should be created
+  domain = "${var.dns_zone}"
+
+  name  = "${var.cluster_name}-workers"
+  type  = "A"
+  ttl   = 300
+  value = "${digitalocean_droplet.mail_worker.ipv4_address}"
 }
 
 # Mail worker droplet instance
